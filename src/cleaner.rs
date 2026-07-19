542fbe77e61b927d4b6c496c87380389733d4bd3
Jan Suthacheeva <jan.suthacheeva@gmail.com>
2026-07-19 17:09:39 +0700

    feat: add whitespace + comment cleaning

---
+++ src/cleaner.rs
@@ -0,0 +1,44 @@
+pub fn clean_program(lines: Vec<&str>) -> Vec<&str> {
+    let mut instructions: Vec<&str> = vec![];
+    let delimiter = "//"; 
+
+    for l in lines {
+        let code = match l.split_once(delimiter) {
+            Some((before, _)) => before.trim(),
+            None => l.trim(),
+        };
+
+        push_if_not_empty(&mut instructions, code);
+    }
+
+    instructions
+}
+
+fn push_if_not_empty<'a>(instructions: &mut Vec<&'a str>, line: &'a str) {
+    if !line.is_empty() {
+        instructions.push(line);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn strips_full_comment_line() {
+        let input = vec!["// Hi", "push constant 1"];
+        assert_eq!(vec!["push constant 1"], clean_program(input));
+    }
+
+    #[test]
+    fn strips_comment_after_line() {
+        let input = vec!["push constant 1 // Hi"];
+        assert_eq!(vec!["push constant 1"], clean_program(input));
+    }
+
+    #[test]
+    fn strips_all_whitespace_around() {
+        let input = vec!["      push constant 1     "];
+        assert_eq!(vec!["push constant 1"], clean_program(input));
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 use std::error::Error;
 
+mod cleaner;
+
 pub fn translate(input: &str) -> Result<String, Box<dyn Error>> {
+
+    let program = cleaner::clean_program(input.lines().collect());
+
     Ok(String::from(""))
 }
