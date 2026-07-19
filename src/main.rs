070648988403c6c436c2779840d1129fe514aee6
Jan Suthacheeva <jan.suthacheeva@gmail.com>
2026-07-19 12:26:07 +0700

    feat: implement main input reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use std::error::Error;
+use std::env;
+use std::process;
+use std::path::{PathBuf};
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
+
+struct Config {
+    input_file: PathBuf,
+    output_file: PathBuf,
+}
+
+impl Config {
+
+    fn build(mut args: impl Iterator<Item = String>) -> Result<Config, Box<dyn Error>> {
+        args.next();
+
+        let input_file = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did not find an input file".into()),
+        };
+
+        let stem = input_file
+            .strip_suffix(".vm")
+            .ok_or("input must be a .vm file")?;
+
+        let output_file = format!("{stem}.asm");
+
+
+        Ok(Config {
+            input_file: input_file.into(),
+            output_file: output_file.into(),
+        })
+    }
 }
