ce48f517ffffa33e1b16ba1d9d8e98b96b9a3ee9
Jan Suthacheeva <jan.suthacheeva@gmail.com>
2026-07-20 16:40:03 +0700

    refactor: throw errors on wrong commands

--- src/parser.rs
+++ src/parser.rs
@@ -23,40 +23,7 @@ pub struct Arithmetic {
     instruction: String,
 }
 
-fn parse_line(line: &str) -> Result<Command, Box<dyn Error>> {
-
-    let elements = line.split_whitespace().count();
-
-    match elements {
-        1 => Ok(Command::Arithmetic(
-            Arithmetic {
-                instruction: String::from(line)
-            }
-            )),
-        3 => {
-            let elements: Vec<&str> = line.split_whitespace().collect();
-            let segment = String::from(elements[1]);
-            let i: u16 = elements[2].parse().unwrap();
-
-            let pp = PushPop {
-                segment,
-                i
-            };
-
-            return match elements[0] {
-                "pop" => Ok(Command::Pop(pp)),
-                "push" => Ok(Command::Push(pp)),
-                _ => Err("Invalid command: {line}".into()),
-            };
-        }
-        _ => Err("Invalid command: {line}".into()),
-    }
-
-
-}
-
 pub fn parse(program: Vec<&str>) -> Result<Vec<Command>, Box<dyn Error>> {
-
     let mut result: Vec<Command> = vec![];
     for line in program {
         let command = parse_line(line)?;
@@ -64,6 +31,59 @@ pub fn parse(program: Vec<&str>) -> Result<Vec<Command>, Box<dyn Error>> {
     }
 
     Ok(result)
+}
+
+fn parse_line(line: &str) -> Result<Command, Box<dyn Error>> {
+
+    let elements = line.split_whitespace();
+
+    match elements.clone().count() {
+        1 => handle_arithmetic_command(line),
+        3 => {
+            handle_memory_command(elements.collect())
+        },
+        _ => Err("Invalid command: {line}".into()),
+    }
+}
+
+fn handle_arithmetic_command(line: &str) -> Result<Command, Box<dyn Error>> {
+    let valid_commands = ["add", "sub", "neg", "eq", "gt", "lt", "and", "or", "not"];
+    if !valid_commands.contains(&line) {
+        return Err("Invalid command: {line}".into());
+    }
+
+    Ok(Command::Arithmetic(
+            Arithmetic {
+                instruction: String::from(line)
+            }
+            )
+        )
+
+}
+
+fn handle_memory_command(elements: Vec<&str>) -> Result<Command, Box<dyn Error>> {
+    let segments = ["local", "argument", "this", "that", "constant", "static", "temp", "pointer"];
+    if !segments.contains(&elements[1]) {
+        return Err("Invalid segment: {segment}".into());
+    }
+    let segment = String::from(elements[1]);
+    let i: u16 = elements[2].parse().unwrap();
+    
+    let pp = PushPop {
+        segment,
+        i
+    };
+    
+    return match elements[0] {
+        "pop" => {
+            match pp.segment.as_str() {
+                "constant" => Err("Invalid: pop constant".into()),
+                _ => Ok(Command::Pop(pp)),
+            }
+        },
+        "push" => Ok(Command::Push(pp)),
+        _ => Err("Invalid command: {line}".into()),
+    };
 }
 
 
@@ -107,9 +127,28 @@ mod tests {
         let input = "push push push this 6";
         assert!(parse_line(input).is_err());
     }
+
     #[test]
     fn errors_on_three_but_unknown() {
         let input = "pushi push push this 6";
         assert!(parse_line(input).is_err());
+    }
+
+    #[test]
+    fn errors_on_pop_constant() {
+        let input = "pop constant 6";
+        assert!(handle_memory_command(input.split_whitespace().collect()).is_err());
+    }
+
+    #[test]
+    fn errors_on_non_valid_push() {
+        let input = "push pushback 6";
+        assert!(handle_memory_command(input.split_whitespace().collect()).is_err());
+    }
+
+    #[test]
+    fn errors_on_non_valid_arithmetic() {
+        let input = "invalid";
+        assert!(handle_arithmetic_command(input).is_err());
     }
 }
