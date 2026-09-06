4ff33194e84e97559622cc687842d153051ec3a7
Jan Suthacheeva <jan.suthacheeva@gmail.com>
2026-09-06 15:59:29 +0700

    io: Add directory handling

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use hack_vm_translator::translate;
 use std::env;
 use std::error::Error;
 use std::fs;
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
 use std::process;
 
 fn main() {
@@ -18,22 +18,32 @@ fn main() {
 }
 
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let input = fs::read_to_string(&config.input_file)?;
+    let mut output = String::from("");
+    for input_file in &config.input_files {
+        let input = fs::read_to_string(input_file)?;
 
-    let file_name = config
-        .input_file
-        .file_stem()
-        .and_then(|x| x.to_str())
-        .ok_or("Error extracting input file name:")?;
+        let name: &str = if input_file.is_relative() {
+            input_file.strip_prefix("./")
+                .expect("could not strip prefix")
+                .to_str().
+                ok_or("Could not transform input file to str")?
+        } else {
+            input_file.to_str().ok_or("Could not transform input file to str")?
+        };
+        output.push_str(&format!("// {name}\n"));
+        let file_output = translate(&input, name)?;
 
-    let output = translate(&input, file_name)?;
+        output.push_str(&file_output);
+        output.push_str("\n");
+    }
+
 
     fs::write(config.output_file, output)?;
     Ok(())
 }
 
 struct Config {
-    input_file: PathBuf,
+    input_files: Vec<PathBuf>,
     output_file: PathBuf,
 }
 
@@ -41,20 +51,49 @@ impl Config {
     fn build(mut args: impl Iterator<Item = String>) -> Result<Config, Box<dyn Error>> {
         args.next();
 
-        let input_file = match args.next() {
+        let input = match args.next() {
             Some(arg) => arg,
-            None => return Err("Did not find an input file".into()),
+            None => return Err("Did not find an input".into()),
         };
 
-        let stem = input_file
-            .strip_suffix(".vm")
-            .ok_or("input must be a .vm file")?;
+        let path = Path::new(&input);
 
-        let output_file = format!("{stem}.asm");
+        if path.is_file() {
+            if path.extension().unwrap() != "vm" {
+                return Err("input must be a directory of .vm file".into());
+            }
 
-        Ok(Config {
-            input_file: input_file.into(),
-            output_file: output_file.into(),
-        })
+            return Ok(Config {
+                input_files: vec![path.to_path_buf()],
+                output_file: path.with_extension("asm").into(),
+            })
+        } 
+        if path.is_dir() {
+            let mut input_files: Vec<PathBuf> = vec![];
+            handle_dir(path, &mut input_files);
+            return Ok(Config {
+                input_files,
+                output_file: path.with_extension("asm").into(),
+            });
+        }
+
+        return Err("test".into());
+        
+    }
+}
+
+fn handle_dir(path: &Path, input_files: &mut Vec<PathBuf>) {
+    for entry in path.read_dir().expect("Failed to read directory") {
+        if let Ok(entry) = entry {
+                let path = entry.path();
+                if path.is_file() {
+                    if path.extension().unwrap() == "vm" {
+                        input_files.push(path.to_path_buf());
+                    }
+                }
+                if path.is_dir() {
+                    handle_dir(&path, input_files);
+                }
+        }
     }
 }
