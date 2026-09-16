c5b6e5574c4936ab721659b98b23fbb4ff7a4283
Jan Suthacheeva <jan.suthacheeva@gmail.com>
2026-09-16 20:59:13 +0700

    initializing: Add calling Sys.init to initialization

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ mod cleaner;
 mod code_writer;
 mod parser;
 
+use crate::parser::{Call, Command};
+
 pub fn translate(input: &str, name: &str) -> Result<String, Box<dyn Error>> {
     let program = cleaner::clean_program(input);
 
@@ -15,5 +17,16 @@ pub fn translate(input: &str, name: &str) -> Result<String, Box<dyn Error>> {
 }
 
 pub fn initialize() -> String {
-    String::from("// initialization\n@256\nD=A\n@SP\nM=D\n\n")
+    let mut res = String::from("// initialization\n@256\nD=A\n@SP\nM=D\n\n");
+
+    let cmd: Vec<Command> = vec![Command::Call(Call{
+        name: String::from("Sys.init"),
+        n_args: 0,
+    })];
+
+    let output = code_writer::translate(cmd, "").expect("Could not initialize translator");
+
+    
+    res.push_str(&output);
+    res
 }
